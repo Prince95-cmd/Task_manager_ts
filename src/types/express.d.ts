41d78dbe43d5412b5a978ba1f245a3363a5f85d7
Prince95-cmd <obiekezieprincec@gmail.com>
2025-10-31 23:58:23 +0100

    Fix TypeScript build errors and add missing type definitions

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -6,11 +6,7 @@ declare global {
             user?: IUserDocument;
             login(user: User, option: {session: false},  done: (err?: any) => void): void;
         }
-    }
-}
 
-declare global {
-    namespace Express {
         export interface User {
             id: string;
             email: string;
